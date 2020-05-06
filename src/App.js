8f00b802a3edc2574e6293faa3c5685e6ffa2954
sarika296 <sarikadewangan94@gmail.com>
2020-05-06 20:59:35 +0530

    search an artist top 10 tracks.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
 import React,{Component} from 'react';
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
 class App extends Component {
-    state = {artistQuery:""};
+    state = {artistQuery:"", artist: null, tracks: []  };
 
     updateArtistQuery = event => {
-        console.log("event.target.value",event.target.value);
-        this.setState({artistQuery:event.target.value});
+        this.setState({ artistQuery: event.target.value });
     }
 
     handleKeyPress = event => {
-        if(event.key==='Enter')
+        if(event.key === 'Enter')
         {
             this.searchArtist();
         }
     }
 
     searchArtist = () => {
-        console.log("this.state",this.state);
-    }
+        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+        .then(response => response.json())
+        .then(json => {
+            console.log('json',json);
+            
+        if(json.artists.total > 0) {
+            const artist = json.artists.items[0];
+            this.setState({ artist });
 
-    render()
-    {
-        return(
+            fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+            .then(response => response.json())
+            .then(json => console.log('tracks json',json))
+            .catch(error => alert(error.message));
+        }
+        })
+        .catch(error => alert(error.message));
+}
+
+    render() {
+        return( 
             <div>
-            <h2>Music Master App</h2>
-            <input 
-                onChange={this.updateArtistQuery}
-                onKeyPress={this.handleKeyPress}
-                placeholder="Search for an Artist" 
-            />
-            <button onClick={this.searchArtist}>Search</button>
+                <h2>Music Master</h2>
+                <input 
+                    onChange={this.updateArtistQuery}
+                    onKeyPress={this.handleKeyPress}
+                    placeholder="Search for an Artist"
+                />
+                <button onClick={this.searchArtist}>Search</button>
             </div>
-        )
+        );
     }
 }
 export default App;  
