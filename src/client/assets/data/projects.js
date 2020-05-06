f8da6f16bab9f31308b85eead1394cae3567673b
sarika296 <sarikadewangan94@gmail.com>
2020-05-06 14:17:17 +0530

    project started.

---
+++ src/client/assets/data/projects.js
@@ -0,0 +1,28 @@
+import project1 from '../images/project/project1.png';
+import project2 from '../images/project/project2.png';
+import project3 from '../images/project/project3.png';
+
+const PROJECTS = [
+    {
+        id : 1,
+        title:'Example React Application',
+        description:"A React App that I built, involving JS and core web dev concepts!",
+        link:'https://github.com/sarika296/portfolio',
+        image:project1
+    },
+    {
+        id : 2,
+        title:'My API',
+        description:"A REST API that I built, from scratch with GET and POST requests!",
+        link:'https://github.com/sarika296/portfolio',
+        image:project2
+    },
+    {
+        id : 3,
+        title:'Operating Systems Final Project',
+        description:"My Unique final project for my university Operating Systems course!",
+        link:'https://github.com/sarika296/portfolio',
+        image:project3
+    }
+];
+export default PROJECTS;
---
+++ src/client/assets/data/socialProfiles.js
@@ -0,0 +1,28 @@
+import emailIcon from '../images/socialProfile/email_icon.png';
+import githubIcon from '../images/socialProfile/github_icon.png';
+import linkedinIcon from '../images/socialProfile/linkedin_icon.png';
+import twitterIcon from '../images/socialProfile/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link:'mailto:sarikadewangan96@gmail.com',
+        image:emailIcon
+    },
+    {
+        id: 2,
+        link:'https://github.com/sarika296',
+        image:githubIcon
+    },
+    {
+        id: 3,
+        link:'https://www.linkedin.com/in/sarika-dewangan',
+        image:linkedinIcon
+    },
+    {
+        id: 4,
+        link:'https://twitter.com/dkcodehelper',
+        image:twitterIcon
+    }
+];
+export default SOCIAL_PROFILES;
