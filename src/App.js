b2d12beac5db13598311f753bae1d53eeb494ef4
sarika296 <sarikadewangan94@gmail.com>
2020-05-06 14:57:09 +0530

    track user input in the state.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React,{Component} from 'react';
 
 class App extends Component {
+    state = {artistQuery:""};
+
+    updateArtistQuery = event => {
+        console.log("event.target.value",event.target.value);
+        this.setState({artistQuery:event.target.value});
+    }
+
+    handleKeyPress = event => {
+        if(event.key==='Enter')
+        {
+            this.searchArtist();
+        }
+    }
+
+    searchArtist = () => {
+        console.log("this.state",this.state);
+    }
+
     render()
     {
         return(
-            <div>Music Master App</div>
+            <div>
+            <h2>Music Master App</h2>
+            <input 
+                onChange={this.updateArtistQuery}
+                onKeyPress={this.handleKeyPress}
+                placeholder="Search for an Artist" 
+            />
+            <button onClick={this.searchArtist}>Search</button>
+            </div>
         )
     }
 }
